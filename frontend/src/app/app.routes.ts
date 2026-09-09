4010c23ed41f9eb2ea626040eccc1ad99918413f
Jean-Dv <jean.valencia01@proton.me>
2026-09-09 11:08:25 -0500

    feat: implement reports module and dashboard page with routing configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./auth/auth.routes').then((r) => r.AUTH_ROUTES),
   },
+  {
+    path: 'reports',
+    loadChildren: () => import('./report/report.routes').then((r) => r.REPORT_ROUTES),
+  },
 ];
